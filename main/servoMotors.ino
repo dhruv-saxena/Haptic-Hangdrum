f86a6b82b83afb4cc11e791ad265b57cf7851b40
dhruv-saxena <dhruv.writeme@gmail.com>
2015-07-22 16:33:26 +0200

    Haptic Hangdrum

---
+++ main/servoMotors.ino
@@ -0,0 +1,25 @@
+void motorTrigger() 
+{ 
+  
+    for(motorPosA = 170; motorPosA>=60; motorPosA-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myServoA.write(motorPosA);              // tell servo to go to position in variable 'pos' 
+    delay(5);                       // waits 5ms for the servo to reach the position 
+     
+} 
+  
+  for(motorPosA = 60; motorPosA < 170; motorPosA += 1)  // goes from 10 degrees to 170 degrees 
+  {                                  // in steps of 1 degree 
+    myServoA.write(motorPosA);     // tell servo to go to position in variable 'pos' 
+    delay(5);              // waits 5ms for the servo to reach the position 
+      
+} 
+
+
+
+} 
+
+void motorTrigger2(){
+    myServoA.write(motorPosA); 
+    myServoB.write(motorPosB); 
+}
